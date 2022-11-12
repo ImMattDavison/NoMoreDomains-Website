d635d15a2912786c69adf9fba6c5d2d6c0ad4078
Matt <matt@spectrebot.net>
2022-11-12 15:18:27 +0000

    add favicon code

--- pages/index.js
+++ pages/index.js
@@ -8,7 +8,12 @@ export default function Home() {
       <Head>
         <title>NoMoreDomains | Put a stop to your unnecessary domain purchases</title>
         <meta name="description" content="NoMoreDomains is a browser extension designed to help you avoid registering new domains that will ultimately end up unused." />
-        <link rel="icon" href="/favicon.ico" />
+        
+		<link rel="icon" href="/assets/favicons/favicon.ico" />
+		<link rel="apple-touch-icon" sizes="180x180" href="/assets/favicons/apple-touch-icon.png" />
+		<link rel="icon" type="image/png" sizes="32x32" href="/assets/favicons/favicon-32x32.png" />
+		<link rel="icon" type="image/png" sizes="16x16" href="/assets/favicons/favicon-16x16.png" />
+		<link rel="manifest" href="/site.webmanifest" />
       </Head>
 
       <main className={styles.main}>
